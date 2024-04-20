f578602e4783b539e2246a204f7ccfefbcde0970
yassinejellouli <testyeees@gmail.com>
2024-04-20 18:24:48 +0100

    1 ajouter la page de profile,2 alimnetr login avec data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import  {BrowserRouter,Routes,Route} from 'react-router-dom';
 import AddEmployeePage from './service-utilisateurs/administrateur/pages/AddEmployeePage.js';
 import AdminDashboardPage from './service-utilisateurs/administrateur/pages/AdminDashboardPage.js';
 import Login from './login-service/login.js';
+import Profile from './service-utilisateurs/Profile.js';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     
     <BrowserRouter>
         <Routes>
+          <Route path='/Profile' element={<Profile/>}></Route>
             <Route path='/login' element={<Login/>}></Route>
             <Route path="/Add-Employee" element={<AddEmployeePage/>}> </Route>
             <Route path="/Dashboard-Admin" element={<AdminDashboardPage/>}></Route>
--- src/login-service/login.js
+++ src/login-service/login.js
@@ -1,28 +1,113 @@
 import './login.css'
+import { useState } from 'react';
 function Login(){
+//partie Admin
+const ChekAdmin =()=>{
+  fetch('http://localhost:8091/Utilisateur-service/List')
+  .then(response => response.json())
+  .then(data => {
+    alert("data receved");
+    // Handle the data received from the server
+    const users = data;
+
+    // Now you can work with the array of users
+    users.forEach(user => {
+      alert("User ID:"+user.id_utilisateur);
+      alert("Name:" +user.nom_utilisateur+ user.prenom_utilisateur);
+      alert("Email:"+ user.email);
+      alert("Pasword" +user.mot_de_passe);
+      // Handle other user properties as needed
+    });
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch
+    console.error('Error fetching data:', error);
+  });
+}
+
+
+//partie Employee
+const ChekEmployee =()=>{
+  fetch('http://localhost:8091/Utilisateur-service/ListEmployee')
+  .then(response => response.json())
+  .then(data => {
+    alert("data receved");
+    // Handle the data received from the server
+    const users = data;
+
+    // Now you can work with the array of users
+    users.forEach(user => {
+      alert("User ID:"+user.id_utilisateur);
+      alert("Name:" +user.nom_utilisateur+ user.prenom_utilisateur);
+      alert("Email:"+ user.email);
+      // Handle other user properties as needed
+    });
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch
+    console.error('Error fetching data:', error);
+  });
+}
+
+
+//partie Chef
+const ChekChef =()=>{
+  fetch('http://localhost:8091/Utilisateur-service/ListChef')
+  .then(response => response.json())
+  .then(data => {
+    alert("data receved");
+    // Handle the data received from the server
+    const users = data;
+
+    // Now you can work with the array of users
+    users.forEach(user => {
+      alert("User ID:"+user.id);
+      alert("Name:" +user.nom+ user.prenom);
+      alert("Email:"+ user.email);
+      // Handle other user properties as needed
+    });
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch
+    console.error('Error fetching data:', error);
+  });
+}
+
+
+
+
+
+
+
+
+
+  const [User, setUser] = useState({
+    email: "",
+    code: ""
+  })
     return(
-        <div class="container ">
+        <div class="container-xl ">
     <div class="row justify-content-center">
 
       <div class="col-md-4">
 
         <form class="login-form justify-content-center">
         <div className='dot'></div>
-
+<br></br>
           <h2 class="text-center mb-4 customtitle">Login</h2>
           
           <div class="form-group">
-            <label for="username">Username:</label>
+            <label for="username">email</label>
             <br></br>
-            <input type="text" class="form-control custominput" id="username" name="username" placeholder="Enter your username" />
+            <input type="text" class="form-control custominput" id="username" name="email" value={User.email} onChange={(e) => setUser({...User, email: e.target.value})} placeholder="Enter your username" />
           </div>
           <div class="form-group">
-            <label for="password">Password:</label>
+            <label for="password">password</label>
             <br></br>
-            <input type="password" class="form-control custominput" id="password" name="password" placeholder="Enter your password" />
+            <input type="password" class="form-control custominput" id="password" name="code"  value={User.code} onChange={(e) => setUser({...User, code: e.target.value})} placeholder="Enter your password" />
           </div>
           <br></br>
-          <button type="submit" class="btn  w-100 custombtn">Login</button>
+          <button type="submit" class="btn  w-100 custombtn" onClick={()=>{ ChekAdmin();alert("hello "+User.email+" ,"+User.code);}}>Login</button>
         <br></br>
         <br></br>
         
