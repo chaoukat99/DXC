d88878b5456fd78ef9976cb998e66c95cd76f6e5
yassinejellouli <testyeees@gmail.com>
2024-04-22 11:54:47 +0100

    login done,profile on hold

--- src/login-service/login.js
+++ src/login-service/login.js
@@ -1,123 +1,138 @@
-import './login.css'
-import { useState } from 'react';
-function Login(){
-//partie Admin
-const ChekAdmin =()=>{
-  fetch('http://localhost:8091/Utilisateur-service/List')
-  .then(response => response.json())
-  .then(data => {
-    alert("data receved");
-    // Handle the data received from the server
-    const users = data;
+import "./login.css";
+import { useState } from "react";
+import CryptoJS from "crypto-js";
 
-    // Now you can work with the array of users
-    users.forEach(user => {
-      alert("User ID:"+user.id_utilisateur);
-      alert("Name:" +user.nom_utilisateur+ user.prenom_utilisateur);
-      alert("Email:"+ user.email);
-      alert("Pasword" +user.mot_de_passe);
-      // Handle other user properties as needed
-    });
-  })
-  .catch(error => {
-    // Handle any errors that occur during the fetch
-    console.error('Error fetching data:', error);
-  });
-}
-
-
-//partie Employee
-const ChekEmployee =()=>{
-  fetch('http://localhost:8091/Utilisateur-service/ListEmployee')
-  .then(response => response.json())
-  .then(data => {
-    alert("data receved");
-    // Handle the data received from the server
-    const users = data;
-
-    // Now you can work with the array of users
-    users.forEach(user => {
-      alert("User ID:"+user.id_utilisateur);
-      alert("Name:" +user.nom_utilisateur+ user.prenom_utilisateur);
-      alert("Email:"+ user.email);
-      // Handle other user properties as needed
-    });
-  })
-  .catch(error => {
-    // Handle any errors that occur during the fetch
-    console.error('Error fetching data:', error);
-  });
-}
-
-
-//partie Chef
-const ChekChef =()=>{
-  fetch('http://localhost:8091/Utilisateur-service/ListChef')
-  .then(response => response.json())
-  .then(data => {
-    alert("data receved");
-    // Handle the data received from the server
-    const users = data;
-
-    // Now you can work with the array of users
-    users.forEach(user => {
-      alert("User ID:"+user.id);
-      alert("Name:" +user.nom+ user.prenom);
-      alert("Email:"+ user.email);
-      // Handle other user properties as needed
-    });
-  })
-  .catch(error => {
-    // Handle any errors that occur during the fetch
-    console.error('Error fetching data:', error);
-  });
-}
-
-
-
-
-
-
-
-
-
+function Login() {
   const [User, setUser] = useState({
     email: "",
-    code: ""
-  })
-    return(
-        <div class="container-xl ">
-    <div class="row justify-content-center">
+    password: "",
+  });
 
-      <div class="col-md-4">
 
-        <form class="login-form justify-content-center">
-        <div className='dot'></div>
-<br></br>
-          <h2 class="text-center mb-4 customtitle">Login</h2>
-          
-          <div class="form-group">
-            <label for="username">email</label>
+  // check tokens ---> get user info ---> create session
+  function Chek() {
+    //encypted
+    var encryptedBase64Key = "bXVzdGJlMTZieXRlc2tleQ==";
+    var parsedBase64Key = CryptoJS.enc.Base64.parse(encryptedBase64Key);
+
+    const encryptedEmail = CryptoJS.AES.encrypt(User.email, parsedBase64Key, {
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7,
+    });
+    const encryptedPassword = CryptoJS.AES.encrypt(
+      User.password,
+      parsedBase64Key,
+      {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7,
+      }
+    );
+
+    //decrypted
+
+    
+   
+   
+    // Construct the encrypted token object
+    const token = {
+      email: encryptedEmail.toString(),
+      password: encryptedPassword.toString()
+    };
+    
+    fetch("http://localhost:8091/Utilisateur-service/login", {
+  method: "POST",
+  headers: {
+    Accept: 'application/json',
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(token)
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  
+  return response.text();
+})
+.then(data => {
+//decrypt user info
+  var decryptedData = CryptoJS.AES.decrypt(data, parsedBase64Key, {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7,
+  });
+    //create session with user info
+
+if(decryptedData.toString(CryptoJS.enc.Utf8).length === 0){
+alert("login or password incorrect");
+}else{
+  sessionStorage.setItem("UserInfo", decryptedData.toString(CryptoJS.enc.Utf8)); 
+
+}
+
+})
+.catch(error => {
+  console.error("Error:", error);
+});
+
+   
+  }
+
+  return (
+    <div className="container-xl ">
+      <div className="row justify-content-center">
+        <div className="col-md-4">
+          <form method="POST" className="login-form justify-content-center">
+            <div className="dot"></div>
             <br></br>
-            <input type="text" class="form-control custominput" id="username" name="email" value={User.email} onChange={(e) => setUser({...User, email: e.target.value})} placeholder="Enter your username" />
-          </div>
-          <div class="form-group">
-            <label for="password">password</label>
+            <h2 className="text-center mb-4 customtitle">Login</h2>
+
+            <div className="form-group">
+              <label>email</label>
+              <br></br>
+              <input
+                type="text"
+                className="form-control custominput"
+                id="username"
+                name="email"
+                value={User.email}
+                onChange={(e) => setUser({ ...User, email: e.target.value })}
+                placeholder="Enter your username"
+              />
+            </div>
+            <div className="form-group">
+              <label >password</label>
+              <br></br>
+              <input
+                type="password"
+                className="form-control custominput"
+                id="password"
+                name="password"
+                value={User.password}
+                onChange={(e) => setUser({ ...User, password: e.target.value })}
+                placeholder="Enter your password"
+              />
+            </div>
             <br></br>
-            <input type="password" class="form-control custominput" id="password" name="code"  value={User.code} onChange={(e) => setUser({...User, code: e.target.value})} placeholder="Enter your password" />
-          </div>
-          <br></br>
-          <button type="submit" class="btn  w-100 custombtn" onClick={()=>{ ChekAdmin();alert("hello "+User.email+" ,"+User.code);}}>Login</button>
-        <br></br>
-        <br></br>
+            <button
+              type="button"
+              className="btn  w-100 custombtn"
+              onClick={() => {
+                Chek();
+               
         
-        <br></br>
-        <br></br>
+              }}
+            >
+              Login
+            </button>
+            <br></br>
+            <br></br>
 
-        </form>
+            <br></br>
+            <br></br>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-    )
+  );
 }
 export default Login;
--- src/service-utilisateurs/Profile.js
+++ src/service-utilisateurs/Profile.js
@@ -1,162 +1,137 @@
-import './css/Profile.css';
-import React from 'react';
-import Header from './administrateur/components/Header/header';
-import { useState } from 'react';
-function Profile(){
-return(
+import "./css/Profile.css";
+import React from "react";
+import Header from "./administrateur/components/Header/header";
+import lg from "../login-service/static/dxc.png";
+import { useState } from "react";
+function Profile() {
+  alert(sessionStorage.getItem('UserInfo'))
+  return (
     <>
-    <Header />
-    <div class="container-fluid emp-profile ">
-    <form method="post">
-        <div class="row ">
-            <div class="col-md-4">
-                <div class="profile-img">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt=""/>
-                    <div class="file btn btn-lg btn-primary">
-                        Change Photo
-                        <input type="file" name="file"/>
-                    </div>
+      <Header />
+
+      <div className="container-fluid w-100">
+        <div className="row">
+          <div className="col-3 p-5">
+            <div>
+              <div class="text-center ProfileIMage">
+                <img
+                  src={lg}
+                  className="rounded-5"
+                  width={"100%"}
+                  height={"100%"}
+                />
+              </div>
+            </div>
+            <div >
+              <input
+                class="form-control form-control-lg"
+                id="formFileLg"
+                type="file"
+              />
+            </div>
+          </div>
+
+          <div className="col-9 p-5">
+            <div class="container-fluid  ">
+              <form method="post">
+                <div class="row ">
+                  <div class="form-group">
+                    <input type="hidden" id="exampleInputEmail1" />
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">
+                      <h5>Adress mail</h5>
+                    </label>
+                    <input
+                      type="email"
+                      class="form-control rounded-5 h-50 text-xl-start "
+                      id="exampleInputEmail1"
+                      placeholder="Enter email"
+                    />
+                  </div>
                 </div>
-            </div>
-            <div class="col-md-6">
-                <div class="profile-head">
-                            <h5>
-                                Kshiti Ghelani
-                            </h5>
-                            <h6>
-                                Web Developer and Designer
-                            </h6>
-                            <p class="proile-rating">RANKINGS : <span>8/10</span></p>
-                    <ul class="nav nav-tabs" id="myTab" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>
-                        </li>
-                    </ul>
+                <br></br>
+                <div class="row ">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">
+                      <h5>Nom</h5>
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control form-control-lg rounded-5 h-50 text-xl-start "
+                      id="exampleInputEmail1"
+                      placeholder="Enter email"
+                    />
+                  </div>
                 </div>
+                <br></br>
+                <div class="row ">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">
+                      <h5>Prenom</h5>
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control form-control-lg rounded-5 h-50 text-xl-start "
+                      id="exampleInputEmail1"
+                      placeholder="Enter email"
+                    />
+                  </div>
+                </div>
+                <br></br>
+                <div class="row ">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">
+                      <h5>Password</h5>
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control form-control-lg rounded-5 h-50 text-xl-start "
+                      id="exampleInputEmail1"
+                      placeholder="Enter email"
+                    />
+                  </div>
+                </div>
+                <br></br>
+                <div class="row ">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">
+                      <h5>Equipe</h5>
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control form-control-lg rounded-5 h-50 text-xl-start "
+                      id="exampleInputEmail1"
+                      placeholder="Enter email"
+                      readonly
+                    />
+                  </div>
+                </div>
+                <br></br>
+                <div class="row ">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">
+                      <h5>Profession</h5>
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control form-control-lg rounded-5 h-50 text-xl-start "
+                      id="exampleInputEmail1"
+                      placeholder="Enter email"
+                      readonly
+                    />
+                  </div>
+                </div>
+               
+                <br></br>
+              </form>
             </div>
-            <div class="col-md-2">
-                <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Edit Profile"/>
-            </div>
+          </div>
         </div>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="profile-work">
-                    <p>WORK LINK</p>
-                    <a href="">Website Link</a><br/>
-                    <a href="">Bootsnipp Profile</a><br/>
-                    <a href="">Bootply Profile</a>
-                    <p>SKILLS</p>
-                    <a href="">Web Designer</a><br/>
-                    <a href="">Web Developer</a><br/>
-                    <a href="">WordPress</a><br/>
-                    <a href="">WooCommerce</a><br/>
-                    <a href="">PHP, .Net</a><br/>
-                </div>
-            </div>
-            <div class="col-md-8">
-                <div class="tab-content profile-tab" id="myTabContent">
-                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>User Id</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>Kshiti123</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Name</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>Kshiti Ghelani</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Email</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>kshitighelani@gmail.com</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Phone</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>123 456 7890</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Profession</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>Web Developer and Designer</p>
-                                    </div>
-                                </div>
-                    </div>
-                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Experience</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>Expert</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Hourly Rate</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>10$/hr</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Total Projects</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>230</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>English Level</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>Expert</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label>Availability</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p>6 months</p>
-                                    </div>
-                                </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <label>Your Bio</label><br/>
-                                <p>Your detail description</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>           
-</div></>
-)
-
-
-
-
+      </div>
+    </>
+  );
 }
+
 export default Profile;
